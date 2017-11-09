29f6a64d1f5eb5f749ff2eac47c40757a266a17b
AntonioMota0014 <32169072+AntonioMota0014@users.noreply.github.com>
2017-11-09 17:30:55 +0000

    Add files via upload

---
+++ Conversor_Temperatura.java
@@ -0,0 +1,16 @@
+package pacote1;
+
+import java.util.Scanner;
+
+public class Conversor_Temperatura {
+
+	public static void main(String[] args) throws Exception {
+	{double C, F;
+	System.out.print("Temperaturas em graus Fahrenheit?");
+	Scanner LerS = new Scanner(System.in);
+	F=LerS.nextDouble();
+	C=(F-32)/1.8;
+	System.out.printf(String.format("%3.1f graus Fahrenheit = %3.1f graus Celsius", F,C));}}}
+	
+	
+
