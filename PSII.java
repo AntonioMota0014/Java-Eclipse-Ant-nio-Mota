c133302fc5479181cc70e7525fac0fce7e9cc188
AntonioMota0014 <32169072+AntonioMota0014@users.noreply.github.com>
2017-11-17 16:39:21 +0000

    Add files via upload

---
+++ PSII.java
@@ -0,0 +1,35 @@
+
+package psii;
+import java.util.Scanner;
+public class PSII {
+  
+public static void main(String[] args) {
+        Scanner ler = new Scanner (System.in);
+        double num1, num2 , num3;
+        double max = 0;
+        System.out.print("Qual o primeiro numero?");
+        num1 =ler.nextDouble();
+        System.out.print("Qual o segundo numero?");
+        num2 = ler.nextDouble();
+        System.out.print("Qual o terceiro numero?");
+        num3 = ler.nextDouble();
+        if (num1>num2 && num1>num3)
+        max = num1;
+        
+        if (num2>num1 && num2>num3)
+        max = num2;
+        if (num3>num1 && num3>num2)
+        max = num3;
+        
+        System.out.print("O máximo é" +max);
+        
+    }
+}
+    
+
+     
+          
+   
+  
+    
+
