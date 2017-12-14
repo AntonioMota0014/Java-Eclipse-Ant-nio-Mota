78298ef1b857a26e5d25380bc0adda800d5f05d7
AntonioMota0014 <32169072+AntonioMota0014@users.noreply.github.com>
2017-12-14 16:17:47 +0000

    Add files via upload

---
+++ R8.java
@@ -0,0 +1,20 @@
+package pacote1;
+
+import java.util.Scanner;
+
+public class R8 {
+
+	public static void main(String[] args) {
+		        int N , Fat=1;
+	       Scanner LerS = new Scanner (System.in);
+	       System.out.print("Digite um inteiro inferior a 32:");
+	       N=LerS.nextInt();
+	   for(int M=N ; M>=1 ; M--)
+	       Fat*=M;
+	   System.out.printf("%d!=%d",N,Fat);
+	            }
+	            
+
+	}
+
+
